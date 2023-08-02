e17024616d3492d98373d7b3414b5de9e3940aff
JosifVukicevic <josifjole32@gmail.com>
2023-08-02 14:33:02 +0200

    Fisrt-commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var clockIcon = document.getElementById('clock-icon');
+var weatherIcon = document.getElementById('weather-icon');
+var wellnessIcon = document.getElementById('wellness-icon');
+var breakIcon = document.getElementById('break-icon');
+var exerciseIcon = document.getElementById('exercise-icon');
+var journalIcon = document.getElementById('journal-icon');
+var photosIcon = document.getElementById('photos-icon');
+
+const bodyELemenet = document.body;
+
+photosIcon.addEventListener('click', () => {
+bodyELemenet.style.backgroundImage = "url('Images\background-2.png')"
+
+});
