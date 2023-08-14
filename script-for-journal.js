23f9a82333271506ab2c518eb58b582fd91e6c70
JosifVukicevic <josifjole32@gmail.com>
2023-08-14 21:11:42 +0200

    bw-commit

---
+++ script-for-journal.js
@@ -0,0 +1,13 @@
+const noteInput = document.getElementById("noteInput");
+const notesContainer = document.querySelector(".notes");
+
+noteInput.addEventListener("keydown", function(event) {
+  if (event.key === "Enter" && noteInput.value.trim() !== "") {
+    const noteText = noteInput.value;
+    const noteElement = document.createElement("div");
+    noteElement.className = "note";
+    noteElement.textContent = noteText;
+    notesContainer.prepend(noteElement);
+    noteInput.value = "";
+  }
+});
--- script.js
+++ script.js
@@ -6,6 +6,8 @@ var exerciseIcon = document.getElementById('exercise-icon');
 var journalIcon = document.getElementById('journal-icon');
 var photosIcon = document.getElementById('photos-icon');
 
+// Promjena pozadine
+
 const bodyELemenet = document.body;
 const imagePaths = ['Images/background-1.webp', 'Images/background-2.webp', 'Images/background-3.webp', 'Images/background-4.webp', 
 'Images/background-5.webp', 'Images/background-6.webp', 'Images/background-7.webp', 'Images/background-8.webp', 
@@ -18,8 +20,9 @@ photosIcon.addEventListener('click', () => {
     bodyELemenet.style.backgroundImage = `url(${imagePath})`;
 });
 
-const time = document.getElementById('time');
+// Prikaz i funkcionalnost sata
 
+const time = document.getElementById('time');
 let isClockVisible = false;
 
 clockIcon.addEventListener('click', () => {
@@ -32,6 +35,8 @@ clockIcon.addEventListener('click', () => {
     }
 });
 
+// Prikaz weather taba
+
 const weatherTab = document.getElementById('weather-tab');
 let isWeatherTabVisible = false;
 
@@ -40,6 +45,8 @@ weatherIcon.addEventListener('click', () => {
     weatherTab.style.visibility = isWeatherTabVisible ? 'visible' : 'hidden';
 });
 
+// Wellness box - prikaz i nestajanje nakon 30 sek
+
 const wellnessTipBox = document.getElementById('wellness-tip-box');
 const wellnessTipContent = document.getElementById('wellness-tip-content');
 
@@ -47,13 +54,14 @@ wellnessIcon.addEventListener('click', () => {
     wellnessTipBox.style.visibility = 'visible';
 
     // Sadrzaj wellness tipa
-    wellnessTipContent.textContent = 'Dnevni wellness tip!';
+    wellnessTipContent.textContent = 'Daily wellness tip!';
 
     setTimeout(() => {
         wellnessTipBox.style.display = 'none';
     }, 30000);
 });
 
+/* Break box */
 
 const breakBox = document.getElementById('break-box');
 const breakOptions = document.getElementById('break-options');
@@ -67,3 +75,70 @@ breakOptions.addEventListener('change', () => {
     alert(`Selected break: ${selectedOption} minutes.`);
     breakBox.style.visibility = 'hidden';
 });
+
+/* Notes container */ 
+
+const notes = document.getElementById('notes-container');
+let isNotesVisible = false;
+notes.style.visibility = 'hidden';
+
+journalIcon.addEventListener('click', () => {
+    isNotesVisible = !isNotesVisible;
+    notes.style.visibility = isNotesVisible ? 'visible' : 'hidden';
+});
+
+if (notes.style.visibility === 'visible') {
+    switch (imagePath) {
+        case "1":
+            notes.style.backgroundColor = "green";
+            break;
+        case "2":
+            notes.style.backgroundColor = "#2e332f";
+            break;
+        case "3":
+            notes.style.backgroundColor = "#c25d2b";
+            break;
+        case "4":
+            notes.style.backgroundColor = "#1c4b57";
+            break;
+        case "5":
+            notes.style.backgroundColor = "#477480";
+            break;
+        case "6":
+            notes.style.backgroundColor = "#243e45";
+            break;
+        case "7":
+            notes.style.backgroundColor = "#c47972";
+            break;
+        case "8":
+            notes.style.backgroundColor = "#bd473c";
+            break;
+        case "9":
+            notes.style.backgroundColor = "#a876a2";
+            break;
+        case "10":
+            notes.style.backgroundColor = "#0c1a0a";
+            break;
+        default:
+            notes.style.backgroundColor = "#0b0b27ee";
+            break;
+    }
+  }
+
+
+  /* ............ */
+   
+  document.addEventListener('DOMContentLoaded', function() {
+    const searchInput = document.getElementById('main-search');
+    const searchForm = document.querySelector('.search-bar');
+  
+    searchForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+      const query = searchInput.value.trim();
+      if (query) {
+        const googleURL = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+        window.open(googleURL, '_blank');
+      }
+    });
+  });
+  
